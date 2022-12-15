7b779d5353450fcfe4d800cbc35bf78916c1882d
Sam Kenney <sam.kenney@louder.com.au>
2022-12-15 22:23:35 +1100

    Refactor python implementation

--- src/dev_cli/lang/python.rs
+++ src/dev_cli/lang/python.rs
@@ -47,6 +47,36 @@ impl Python {
     }
 }
 
+/// Parse the response from the request.
+/// If the response is 200, write the file to the directory.
+/// Otherwise, print an error message.
+///
+/// # Arguments
+/// * `response` - The response from the request.
+/// * `file` - The file name to write.
+/// * `dir` - The directory to write the file to.
+///
+/// # Example
+/// ```
+/// use dev_cli::lang::Python;
+/// use dev_cli::request;
+///
+/// let url: String = "https://bitbucket.org/louder/python-template-public/raw/main/README.md".to_string();
+/// let response = request::get(url).await;
+/// parse_response(response, "README.md".to_string(), ".".to_string()).await;
+/// ```
+async fn parse_response(response: reqwest::Response, file: String, dir: String) {
+    match response.status().as_str() {
+        "200" => {
+            let content: String = response.text().await.unwrap_or_default();
+            let file = File::new(file, dir, content);
+            file.write();
+            println!("{} created.", file.name)
+        },
+        _ => println!("Error: Could not get {} from repo.", file),
+    }
+}
+
 /// Implement the Language trait for the Python struct.
 #[async_trait]
 impl Language for Python {
@@ -56,18 +86,9 @@ impl Language for Python {
     /// * `dir` - The directory to write the files to.
     async fn execute(&self, dir: &String) {
         for file in self.files.iter() {
-            let response = request::get(format!("{}{}", self.base_url, file)).await;
-            match response.status().as_str() {
-                "200" => {
-                    let content: String = response.text().await.unwrap();
-                    File::new(file.to_string(), dir.to_string(), content).write();
-                    println!("{} created.", file);
-                }
-                _ => {
-                    println!("Error: Could not get {} from repo.", file);
-                    continue;
-                }
-            }
+            let url: String = format!("{}{}", self.base_url, file);
+            let response: reqwest::Response = request::get(url).await;
+            parse_response(response, file.to_string(), dir.to_string()).await;
         }
     }
 }
