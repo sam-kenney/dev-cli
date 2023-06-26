62de7263d16575d976ed682c43c9e0a82ed4ce4b
Sam Kenney <sam.kenney@louder.com.au>
2023-06-26 16:38:10 +1000

    Update python projects

--- src/cli/lang/python.rs
+++ src/cli/lang/python.rs
@@ -7,15 +7,20 @@ use crate::cli;
 /// * `name` - The name of the project.
 pub async fn python(name: String) {
     println!("Creating Python project `{}`", name);
-    let base_url: &str = "https://bitbucket.org/louder/python-template-public/raw/main/";
+    let base_url: &str = "https://raw.githubusercontent.com/sam-kenney/python-template/main/";
     let files: Vec<&str> = vec![
         ".editorconfig",
         ".gitignore",
         ".pre-commit-config.yaml",
         "README.md",
-        "noxfile.py",
-        "requirements-dev.txt",
-        "setup.cfg",
+        "poetry.lock",
+        "pyproject.toml",
+        "src/main.py",
+        "src/__init__.py",
+        "tests/__init__.py",
+        ".github/workflows/github-actions-black.yml",
+        ".github/workflows/github-actions-mypy.yml",
+        ".github/workflows/github-actions-ruff.yml",
     ];
 
     cli::download::download_files(base_url, files, &name).await;
